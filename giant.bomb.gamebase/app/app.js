a8e076153d01be14d7b92af2650c0d207dad00ab
shonie <starnikov2009@ukr.net>
2016-07-26 01:12:03 +0300

    little refactoring in app.js:
    deleted unneccessary variables, comments and console.log's

--- giant.bomb.gamebase/app/app.js
+++ giant.bomb.gamebase/app/app.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const baseUrl = "http://www.giantbomb.com/api/game/";
-var gameId;
-var format = "/?format=json";
-const apiKey = "&api_key=aaba549e362b68c090bbc073b0bde1cf799d1468";
-const fieldList = "&field_list=name,description,deck,id,original_release_date,image,images,genres,platforms,publishers,developers,videos,releases,api_detail_url,site_detail_url";
-
 const data = require('./games_ids/ps4-games-ids.json');
 const fs = require("fs");
 const underscore = require("underscore");
@@ -13,6 +7,11 @@ const _ = require('lodash');
 const request = require('request');
 const jsonfile = require('jsonfile');
 const async = require('async');
+
+const baseUrl = "http://www.giantbomb.com/api/game/";
+const format = "/?format=json";
+const apiKey = "&api_key=aaba549e362b68c090bbc073b0bde1cf799d1468";
+const fieldList = "&field_list=name,description,deck,id,original_release_date,image,images,genres,platforms,publishers,developers,videos,releases,api_detail_url,site_detail_url";
 
 var idsArr = [];
 var gamesArrOutput = [];
@@ -31,13 +30,11 @@ async.each(idsArr, function requestGameObject() {
                 if(err) throw err;
                 console.log('Wrote!');
             })
-
-            //console.log(body);
         }
     })
 }, function callback(error, done) {
         if (error) {
-            console.log('Access restricted!')
+            console.log('Access restricted!');
             throw error;
         }
         console.log('Hacker!');
@@ -46,51 +43,6 @@ async.each(idsArr, function requestGameObject() {
 );
 
 
-//for (var key in gamesArr) {
-//    gameId = key;
-//    var gameFileName = './games_files/' + gameId + '.json';
-//    request(baseUrl + gameId + format + apiKey + fieldList, function (error, response, body) {
-//
-//        if (error) throw error;
-//
-//        if (response.statusCode === 200) {
-//            //fs.writeFileSync('./games_files/games.json', JSON.stringify(JSON.parse(body).results), "UTF-8", (err) => {
-//            //    if (err) throw err;
-//            //console.log('appended');    this works too, but this overwrite the existing text
-//            //});
-//
-//
-//            //console.log(JSON.parse(body).results.genres);   this works
-//            //    fs.writeFile(gameFileName, '', (err) => {
-//            //        if (err) throw err;
-//            //    console.log('It\'s saved!');
-//            //});
-//
-//            //fs.appendFile('./games_files/games.json', JSON.parse(body).results, (err) => {
-//            //    if (err) throw err;
-//
-//            //fs.appendFile('./games_files/games.json', '/n', (err) => {
-//            //    if (err) throw err;
-//            //console.log('New line was successfully appended!');
-//            //});
-//            //console.log('The "data to append" was appended to file!');
-//        }
-//    });
-//}
-
-
-//_.forEach(data.results, function (game) {
-//    request(endPoint + game.id + format + apiKey + fieldList, function (error, resp, body) {
-//        if (!error && resp.statusCode === 200){
-//            jsonfile.writeFile("./result.json", JSON.parse(body), {spaces: 2}, function (err) {
-//                if (err) {
-//                console.error(err);
-//            }
-//        })
-//            //console.log(JSON.parse(body).results.genres);
-//        }
-//    } );
-//} );
 
 
 
