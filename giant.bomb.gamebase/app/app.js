fab2b9020b3f1add78a95c7e076b40f16e980d40
shonie <starnikov2009@ukr.net>
2016-07-26 02:07:20 +0300

    as giantbomb implement restrictions in frequencie of requests (only 100 per user per hour), i had to add the setTimeout(request, 3600000ms) to my loop.

--- giant.bomb.gamebase/app/app.js
+++ giant.bomb.gamebase/app/app.js
@@ -23,15 +23,31 @@ for (var i = 0; i < data.length; i++) {
 
 i = -1;
 async.each(idsArr, function requestGameObject() {
+
     i++;
-    request(baseUrl + idsArr[i] + format + apiKey + fieldList, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            fs.appendFile('./games/' + idsArr[i] + '.json', JSON.stringify(JSON.parse(body).results) + ",", (err) => {
-                if(err) throw err;
+    if (i % 100 === 0 && i !== 0) {
+        setTimeout(
+        function requestGames() {
+            request(baseUrl + idsArr[i] + format + apiKey + fieldList, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    fs.appendFile('./games/ps4-games.json', JSON.stringify(JSON.parse(body).results) + ",", (err) => {
+                        if(err) throw err;
+                    console.log('Wrote!');
+                })
+                }
+            })
+        }, 3600000)
+    } else {
+        request(baseUrl + idsArr[i] + format + apiKey + fieldList, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                fs.appendFile('./games/ps4-games.json', JSON.stringify(JSON.parse(body).results) + ",", (err) => {
+                    if(err) throw err;
                 console.log('Wrote!');
             })
-        }
-    })
+            }
+        })
+    }
+
 }, function callback(error, done) {
         if (error) {
             console.log('Access restricted!');
